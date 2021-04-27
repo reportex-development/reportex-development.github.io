3f5206462e0cbcbcd4ba4781dab7dd0388525bd0
MonikaS <monika.silvia.s@hotmail.ca>
2021-04-27 14:21:03 -0700

    Reverted back to original console

--- Lobby-menu/system-02-30cbee4ff5.js
+++ Lobby-menu/system-02-30cbee4ff5.js
@@ -14173,8 +14173,8 @@ var TextEditorImageInput = function (_Handsontable$editors) {
         scheduleNameElm.classList.add("schedule-name"), scheduleNameElm.textContent = schedule.name, scheduleContainerElm.appendChild(scheduleNameElm);
         var scheduleTimeElm = document.createElement("span");
 		
-		var posOne = document.getElementsByClassName("schedule-name")[0]);
-		console.log(posOne);
+		
+		console.log(document.getElementsByClassName("schedule-name")[0]);
 		
         return scheduleTimeElm.classList.add("schedule-time"), scheduleTimeElm.textContent = schedule.startDateLabel + " - " + schedule.endDateLabel, scheduleContainerElm.appendChild(scheduleTimeElm), scheduleElm;
       }
