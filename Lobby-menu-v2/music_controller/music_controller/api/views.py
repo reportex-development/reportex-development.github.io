1109b081f6bb95f4a44a895a97b91eb5464ed63a
MonikaS <monika.silvia.s@hotmail.ca>
2021-04-28 11:58:40 -0700

    Have to start from scratch again
    Python code was not working. I will need to fully uninstall the programs and reinstall them.

--- Lobby-menu-v2/music_controller/music_controller/api/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here. Code Responsible for rendering the views.
-def main(request):
-    return HttpResponse("Hello")
