40fe0f32a5016e2c4310f3da8413f03da21fb4d3
MonikaS <monika.silvia.s@hotmail.ca>
2021-04-28 11:08:09 -0700

    Setting up Django project for Lobby sign

---
+++ Lobby-menu-v2/music_controller/api/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+# Create your views here. Code that is responsible for rendeirng our view.
+#location on the webserver that you are going to will be written here.
