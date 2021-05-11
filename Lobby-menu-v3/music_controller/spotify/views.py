3215070331f51234e610016979d8bc2ee7c6c0be
MonikaS <monika.silvia.s@hotmail.ca>
2021-05-10 17:38:49 -0700

    Started Tutorial #13 but start at 20mins

---
+++ Lobby-menu-v3/music_controller/spotify/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .credentials import REDIRECT_URI, CLIENT_SECRET, CLIENT_ID
+from rest_framework.views import APIView
+from requests import Request, post
+from rest_framework import status
+from rest_framework.response import Response
+
+class AuthURL(APIView):
+    def get(self, request, format=None):
+        scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+
+        url = Request('GET', 'https://accounts.spotify.com/authorize', parama={
+            'scope': scopes,
+            'response_type': 'code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+        }).prepare().url
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
+
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
