c6224e2de11c7a355d4c8afe450d82bd7e83a616
MonikaS <monika.silvia.s@hotmail.ca>
2021-05-17 16:59:04 -0700

    Transferred
    Transferred data and code to a new repository:
    
    https://github.com/reportex-development/Lobby-Menu
    
    Now keeping this web pages as bare as possible. Will add a react index page and then a styles sheet in a folder for the Crestron Scheduler in the next few changes.

--- Lobby-menu-v3/music_controller/spotify/credentials.py
+++
@@ -1,3 +0,0 @@
-CLIENT_ID = "8ea6789bd87a410c9cc7348d41222efd"
-CLIENT_SECRET = "7dfcee8d46564596b7ced3f69ef80fe3"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
--- Lobby-menu/assets/javascript/break-hide.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByClassName("schedule-name").style.display = "none";
-
-var str = "";
-var n = str.search("</br>");
-document.getElementsByClassName("schedule-name").innerHTML = n;
