dc589b4e8472301c1364aa8ae22c0ff36cc29add
MonikaS <83093087+reportex-development@users.noreply.github.com>
2021-05-21 09:12:41 -0700

    Fixing code.

--- Banner.js
+++ Banner.js
@@ -1,18 +1,25 @@
+  
 class Banner extends React.Component {
-    constructor(){
+    constructor(props){
         super(props);
         this.state = {
-            name: '',
-            appVersion: ''
+            name: 'J',
+            apppVersion: '1'
         }
     }
 
     render(){
         return(
             <>
-                <h2>Hello Friend! Welcome back.</h2>
-                <button>Download Button</button>
+                <h2>Hello {this.state.name || 'Friend'}! Welcome Back.</h2>
+                {
+                    this.state.apppVersion && this.state.apppVersion < 2
+                    ? <p>Your app is out of date. Please download the new version and take a look at all the new features.</p> 
+                    : ''
+                }
+                <CoolButton customText={this.state.apppVersion && this.state.apppVersion < 2 ? 'Download v2' : 'Download'} />
             </>
         )
     }
+
 }
